37c22e5f8bd13828619b7e16599947910e90ddfe
ramesh0595 <rameshchinnu509@gmail.com>
2026-09-04 08:44:54 +0530

    Correction Hooks

--- tests/Stepdefinition/HooksClass.js
+++ tests/Stepdefinition/HooksClass.js
@@ -9,23 +9,23 @@ Before(async function () {
 });
 
 After(async function (Scenario) {
-  // await this.page.screenshot({
-  //   path:`tests/Screenshots/"+Date.now()+".png`,
-  //   fullPage:true
-  // })
+  await this.page.screenshot({
+    path:`tests/Screenshots/"+Date.now()+".png`,
+    fullPage:true
+  })
 
-  // //Take Screenshot all Passed and Failed Scenarios
-  // const scenarioName = Scenario.pickle.name
-  //   .trim()
-  //   .replace(/[^a-zA-Z0-9]/g, "_");
-  // const filePath = `tests/Screenshots/${scenarioName}.png`;
+  //Take Screenshot all Passed and Failed Scenarios
+  const scenarioName = Scenario.pickle.name
+    .trim()
+    .replace(/[^a-zA-Z0-9]/g, "_");
+  const filePath = `tests/Screenshots/${scenarioName}.png`;
 
-  // const scenarioScreeshot=await this.page.screenshot({
-  //   path: filePath,
-  //   fullPage: true,
-  // });
+  const scenarioScreeshot=await this.page.screenshot({
+    path: filePath,
+    fullPage: true,
+  });
 
-  // await this.attach(scenarioScreeshot,'image/png')
+  await this.attach(scenarioScreeshot,'image/png')
 
   if(Scenario.result.status==='FAILED'){
     //Take Screenshot all Failed Scenarios
@@ -43,71 +43,3 @@ After(async function (Scenario) {
   }
   await utils.pageClose(this.page);
 });
-
-//Ordered Hooks
-// Before({order:0},async function () {
-//  console.log("--------Before Scenario----------1")
-// });
-
-// After({order:1},async function () {
-//   console.log("--------After Scenario----------1")
-// });
-
-// Before({order:1},async function () {
-//  console.log("--------Before Scenario----------2")
-// });
-
-// After({order:0},async function () {
-//   console.log("--------After Scenario----------2")
-// });
-
-//Tagged Hooks
-// Before({tags:"@smoke"},async function () {
-//  console.log("--------Before Scenario----------1")
-// });
-
-// After({tags:"@smoke"},async function () {
-//   console.log("--------After Scenario----------1")
-// });
-
-// Before({tags:"@retest"},async function () {
-//  console.log("--------Before Scenario----------2")
-// });
-
-// After({tags:"@retest"},async function () {
-//   console.log("--------After Scenario----------2")
-// });
-
-// //Ordered and Tagged Hooks
-// Before({tags:"@smoke",order:0},async function () {
-//  console.log("--------Before Scenario----------1")
-// });
-
-// After({tags:"@smoke",order:0},async function () {
-//   console.log("--------After Scenario----------1")
-// });
-
-// Before({order:1},async function () {
-//  console.log("--------Before Scenario----------2")
-// });
-
-// After({order:1},async function () {
-//   console.log("--------After Scenario----------2")
-// });
-
-//Ordered and Multiple Tagged Hooks
-// Before({tags:"@smoke or @retest",order:0},async function () {
-//  console.log("--------Before Scenario----------1")
-// });
-
-// After({tags:"@smoke or @retest",order:0},async function () {
-//   console.log("--------After Scenario----------1")
-// });
-
-// Before({order:1},async function () {
-//  console.log("--------Before Scenario----------2")
-// });
-
-// After({order:1},async function () {
-//   console.log("--------After Scenario----------2")
-// });
